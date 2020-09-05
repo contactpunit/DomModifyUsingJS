a185c94ed1c5596ed126f954dfb1f57f1d14a889
Punit Jain <contactpunitjain@gmail.com>
2020-09-05 18:37:08 +0530

    added code to control toggle of backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+}
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+}
+
+const backdropMovieHandler = () => {
+    toggleMovieModal();
+}
+
+const cancelAddMovieHandler = () => {
+    toggleMovieModal();
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropMovieHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
